f4e6447077e979ade831b08c1f03004a8770e96c
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-04-18 10:37:42 +0300

    Update include:   - Add a section that lists the currently available learning materials for people who do not want to view the cards listed above   - Add validations in the learning materials model for the title, description and link   - Refactor error message localization in LearningMaterial model to improve code maintainability

--- app/models/learning_material.rb
+++ app/models/learning_material.rb
@@ -7,4 +7,9 @@ class LearningMaterial < ApplicationRecord
 
   # Enum
   enum tag: { beginners: 0, intermediates: 1, experts: 2 }
+
+  # Validations
+  validates :tag, :learning_material_title, :learning_material_description, :learning_material_link, presence: true
+  validates :learning_material_link,
+            format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: :invalid_url }
 end
